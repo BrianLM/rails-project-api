4b9a3a8bd7db7b58762fa9d6b9594d2168fd0501
BrianLM <brian.distefano@libertymutual.com>
2017-10-16 16:29:28 -0400

    Migrate Store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016202140) do
+ActiveRecord::Schema.define(version: 20171016202817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20171016202140) do
 
   create_table "lists", force: :cascade do |t|
     t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
