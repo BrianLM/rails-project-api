53a2b56ed28da70484267a40991523f8ed62fca3
BrianLM <brian.distefano@libertymutual.com>
2017-10-16 16:32:39 -0400

    Migrate List_Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016202817) do
+ActiveRecord::Schema.define(version: 20171016203147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,17 @@ ActiveRecord::Schema.define(version: 20171016202817) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "list_items", force: :cascade do |t|
+    t.boolean "purchased"
+    t.integer "quantity"
+    t.bigint "list_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_list_items_on_item_id"
+    t.index ["list_id"], name: "index_list_items_on_list_id"
+  end
+
   create_table "lists", force: :cascade do |t|
     t.boolean "active"
     t.datetime "created_at", null: false
@@ -72,4 +83,6 @@ ActiveRecord::Schema.define(version: 20171016202817) do
   add_foreign_key "examples", "users"
   add_foreign_key "group_memberships", "groups"
   add_foreign_key "group_memberships", "users"
+  add_foreign_key "list_items", "items"
+  add_foreign_key "list_items", "lists"
 end
