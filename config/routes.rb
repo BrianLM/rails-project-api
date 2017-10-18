01c51bd0b10bb1d63a8490632106235333f92239
BrianLM <brian.distefano@libertymutual.com>
2017-10-18 16:30:40 -0400

    Edit routes
    Edit routes to exclude new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :purchases
-  resources :prices
-  resources :list_items
-  resources :stores
-  resources :group_memberships
-  resources :groups
-  resources :lists
-  resources :items
+  resources :purchases, except: %i[new edit]
+  resources :prices, except: %i[new edit]
+  resources :list_items, except: %i[new edit]
+  resources :stores, except: %i[new edit]
+  resources :group_memberships, except: %i[new edit]
+  resources :groups, except: %i[new edit]
+  resources :lists, except: %i[new edit]
+  resources :items, except: %i[new edit]
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
