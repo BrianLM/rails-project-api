740c05f7c4d83a83330a5d3855cbe6876f7ad442
BrianLM <brian.distefano@libertymutual.com>
2017-10-18 16:33:44 -0400

    Add deleted files
    Commit deleted file reference.
    Add scripts/list/post.sh

--- db/migrate/20171018201154_add_group_to_list.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupToList < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :lists, :group, foreign_key: true
-  end
-end
---
+++ scripts/lists/post.sh
@@ -0,0 +1,12 @@
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/lists"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "example": {
+      "text": "'"${TEXT}"'"
+    }
+  }'
