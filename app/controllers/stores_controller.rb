a3218798976223b538490fa2d4c178da9c8d9e10
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 14:58:47 -0400

    Modify StoresController
    Change inheritance to ProtectedController

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class StoresController < ApplicationController
+class StoresController < ProtectedController
   before_action :set_store, only: %i[show update destroy]
 
   # GET /stores
