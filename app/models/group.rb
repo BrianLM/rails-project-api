3a7f148336aea856356d824e9bd805f332a6d52a
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 13:33:56 -0400

    Add group definitions
    Add has many group_memberships
    Add has many users through group_memberships
    Add belongs_to user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :group_memberships
+  has_many :users, through: :group_memberships
+  validates :name, :user, presence: true
 end
