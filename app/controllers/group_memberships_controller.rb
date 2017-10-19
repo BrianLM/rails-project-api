f2920e3ea3468e69ec39b4dde64977747ca78ed5
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 15:37:50 -0400

    Modify GroupMembershipsController
    Add current user validation

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,9 +1,11 @@
-class GroupMembershipsController < ApplicationController
-  before_action :set_group_membership, only: [:show, :update, :destroy]
+# frozen_string_literal: true
+
+class GroupMembershipsController < ProtectedController
+  before_action :set_group_membership, only: %i[show update destroy]
 
   # GET /group_memberships
   def index
-    @group_memberships = GroupMembership.all
+    @group_memberships = current_user.group_memberships.all
 
     render json: @group_memberships
   end
@@ -15,10 +17,12 @@ class GroupMembershipsController < ApplicationController
 
   # POST /group_memberships
   def create
-    @group_membership = GroupMembership.new(group_membership_params)
+    @group_membership = current_user.group_memberships
+                                    .build(group_membership_params)
 
     if @group_membership.save
-      render json: @group_membership, status: :created, location: @group_membership
+      render json: @group_membership,
+             status: :created, location: @group_membership
     else
       render json: @group_membership.errors, status: :unprocessable_entity
     end
@@ -38,14 +42,15 @@ class GroupMembershipsController < ApplicationController
     @group_membership.destroy
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group_membership
-      @group_membership = GroupMembership.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group_membership
+    @group_membership = current_user.group_memberships.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def group_membership_params
-      params.require(:group_membership).permit(:group_id, :user_id)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def group_membership_params
+    params.require(:group_membership).permit(:group_id, :user_id)
+  end
+
+  private :set_group_membership, :group_membership_params
 end
