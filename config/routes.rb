06e0ebadf445f4d1ebecf337c7a915fd09d81543
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 16:01:04 -0400

    Fix linter comment
    Fix long line linter error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   post '/new_list' => 'lists#listwitems'
   post '/myitems' => 'items#indexuser'
   post '/mystores' => 'stores#indexuser'
-  match 'invite/:id' => 'users#showemail', :constraints => { id: /[0-z\.]+/ }, :via => [:get]
+  match 'invite/:id' => 'users#showemail',
+        :constraints => { id: /[0-z\.]+/ }, :via => [:get]
 end
