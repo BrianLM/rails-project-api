aca1a7fea8141fbb4cbf0d338aa32c633cb49d84
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 13:31:19 -0400

    Add validates to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
   has_many :list_items
   has_many :list_items, through: :lists
+  validates :name, :unit, :unit_quantity, presence: true
 end
