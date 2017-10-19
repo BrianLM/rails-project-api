d86dea0282bcabf1dcfe2eb64c07c58296057663
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 13:28:08 -0400

    Add validates to Purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Purchase < ApplicationRecord
   belongs_to :item
   belongs_to :user
+  validates :frequency, :number, :mean_price, :item, :user, presence: true
 end
