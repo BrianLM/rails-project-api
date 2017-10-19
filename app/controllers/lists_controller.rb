0725cf80cdf7edfc32269b8c420e06904418f005
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 13:39:56 -0400

    Add current_user to lists controller
    Add current_user to create
    Add current_user to set_list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ListsController < ProtectedController
-  before_action :set_list, only: [:show, :update, :destroy]
+  before_action :set_list, only: %i[show update destroy]
 
   # GET /lists
   def index
@@ -15,7 +17,7 @@ class ListsController < ProtectedController
 
   # POST /lists
   def create
-    @list = List.new(list_params)
+    @list = current_user.list.build(list_params)
 
     if @list.save
       render json: @list, status: :created, location: @list
@@ -58,7 +60,7 @@ class ListsController < ProtectedController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_list
-    @list = List.find(params[:id])
+    @list = current_user.lists.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
