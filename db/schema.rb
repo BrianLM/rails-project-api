d4c17357de29cfcfe316c1fb7e2c28cafb3c7a5f
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 13:14:48 -0400

    Add ERD changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019145742) do
+ActiveRecord::Schema.define(version: 20171019171302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20171019145742) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -44,6 +46,8 @@ ActiveRecord::Schema.define(version: 20171019145742) do
     t.integer "unit_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "list_items", force: :cascade do |t|
@@ -72,8 +76,10 @@ ActiveRecord::Schema.define(version: 20171019145742) do
     t.bigint "store_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["item_id"], name: "index_prices_on_item_id"
     t.index ["store_id"], name: "index_prices_on_store_id"
+    t.index ["user_id"], name: "index_prices_on_user_id"
   end
 
   create_table "purchases", force: :cascade do |t|
@@ -96,6 +102,8 @@ ActiveRecord::Schema.define(version: 20171019145742) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -111,12 +119,16 @@ ActiveRecord::Schema.define(version: 20171019145742) do
   add_foreign_key "examples", "users"
   add_foreign_key "group_memberships", "groups"
   add_foreign_key "group_memberships", "users"
+  add_foreign_key "groups", "users"
+  add_foreign_key "items", "users"
   add_foreign_key "list_items", "items"
   add_foreign_key "list_items", "lists"
   add_foreign_key "lists", "users"
   add_foreign_key "prices", "items"
   add_foreign_key "prices", "stores"
+  add_foreign_key "prices", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "stores"
   add_foreign_key "purchases", "users"
+  add_foreign_key "stores", "users"
 end
