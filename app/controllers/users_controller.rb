b60da5f321ba5be4aa2a2c3ec25b94b7e312310b
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 16:00:19 -0400

    Add find email to UsersController
    Add Find by email via showemail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ProtectedController
   end
 
   def showemail
-    user = User.where('email LIKE ?', "#{params[:id]}%")
+    user = User.where(email: params[:id])
     render json: user
   end
 
