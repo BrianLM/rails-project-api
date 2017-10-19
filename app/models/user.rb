5f8db68b13b17b0a67218397fe135898a34adf57
BrianLM <brian.distefano@libertymutual.com>
2017-10-19 13:21:59 -0400

    Add user model links

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@
 class User < ApplicationRecord
   include Authentication
   has_many :examples
-  has_many :lists
+  has_many :lists, dependent: :destroy
+  has_many :items
+  has_many :stores
+  has_many :groups
+  has_many :prices
+  has_many :group_memberships
+  has_many :purchases
 end
