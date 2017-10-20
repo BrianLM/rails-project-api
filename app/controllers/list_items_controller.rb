93b440aa25e4c713e4f554a2016a62eb125978f6
BrianLM <brian.distefano@libertymutual.com>
2017-10-20 16:54:00 -0400

    List serializer fix
    List items controller remove list requirement.

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -43,7 +43,6 @@ class ListItemsController < ProtectedController
 
   # DELETE /list_items/1
   def destroy
-    head :no_content unless current_user.lists.find(list_item_params[:list_id])
     @list_item.destroy
     head :no_content
   end
--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,4 +1,4 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :active
+  attributes :id, :active, :name, :updated_at
   has_many :list_items
 end
